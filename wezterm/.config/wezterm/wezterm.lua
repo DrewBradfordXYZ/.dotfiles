5608e7d37a875c8d9d9641252c3767b121b97ff6
Drew Bradford <16455249+DrewBradfordXYZ@users.noreply.github.com>
2024-03-16 20:33:56 -0400

    wezterm tab styling

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -193,17 +193,45 @@ config.keys = {
 		mods = "CTRL",
 		action = wezterm.action.DisableDefaultAssignment,
 	},
-
-	-- { key = "h", mods = "ctrl", action = act.ActivateTabRelative(-1) },
-	-- { key = "l", mods = "ctrl", action = act.ActivateTabRelative(1) },
 }
--- Activate the tab specified by the argument value. eg: 0 activates the leftmost tab, while 1 activates the second tab from the left, and so on.
+
+-- Tab config
+config.enable_tab_bar = true
+config.hide_tab_bar_if_only_one_tab = true
+config.tab_bar_at_bottom = true
+config.window_padding = {
+	left = 2,
+	right = 2,
+	top = 0,
+	bottom = 0,
+}
+config.window_frame = {
+	-- The size of the font in the tab bar.
+	-- Default to 10.0 on Windows but 12.0 on other systems
+	font_size = 11.0,
+
+	-- The overall background color of the tab bar when
+	-- the window is focused
+	active_titlebar_bg = "#333333",
+
+	-- The overall background color of the tab bar when
+	-- the window is not focused
+	inactive_titlebar_bg = "#333333",
+}
+config.colors = {
+	tab_bar = {
+		-- The color of the inactive tab bar edge/divider
+		inactive_tab_edge = "#575757",
+	},
+}
+
+-- CTRL+SHIFT + number to activate that tab
 for i = 1, 8 do
-	-- CTRL+SHIFT + number to activate that tab
 	table.insert(config.keys, {
 		key = tostring(i),
 		mods = "CTRL|ALT",
 		action = act.ActivateTab(i - 1),
 	})
 end
+
 return config
